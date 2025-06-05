77b7f3092b434c76e7bd4b896d403867e28c62f1
Siuungsal2 <unggar2pengincar@gmail.com>
2025-06-05 14:30:19 +0700

    Add Database

---
+++ DevHabit/DevHabit.api/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevHabit.api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
